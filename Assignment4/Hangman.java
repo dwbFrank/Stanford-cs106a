8018dc276a7adb42e90eedfea6af1df87e88c910
WEIBING DING <frank@WEIBINGs-iMac.local>
2018-10-02 02:31:56 +0900

    Complete console program

---
+++ Assignment4/Hangman.java
@@ -0,0 +1,134 @@
+
+/*
+ * File: Hangman.java
+ * ------------------
+ * This program will eventually play the Hangman game from
+ * Assignment #4.
+ */
+
+import acm.graphics.*;
+import acm.program.*;
+import acm.util.*;
+
+import java.awt.*;
+
+public class Hangman extends ConsoleProgram {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see acm.program.ConsoleProgram#init()
+	 * 
+	 * add a graphic window next to the console.
+	 */
+	public void init() {
+		canvas = new HangmanCanvas();
+		add(canvas);
+	}
+
+	public void run() {
+		/* You fill this in */
+		println("Welcome to Hangman!");
+		initGame();
+		while (true) {
+			if (isWin()) {
+				winPrint();
+				break;
+			}
+			if (isLose()) {
+				losePrint();
+				break;
+			}
+			printState();
+			String guess = readLine("Your guess: ").toUpperCase();
+			if (isValidGuess(guess)) {
+				gameUpdate(guess.charAt(0));
+			} else {
+				println("Invalid input");
+			}
+		}
+
+	}
+
+	/*
+	 * Initial the game
+	 */
+	private void initGame() {
+		// A random integer generator.
+		RandomGenerator rg = RandomGenerator.getInstance();
+		word = lexicon.getWord(rg.nextInt(10));
+		dashLine = initDashLine(word);
+		numberOfWrongGuess = 0;
+	}
+
+	private void printState() {
+		println("The word now looks like this: " + dashLine);
+		int remainChance = NUMBER_OF_CHANCE - numberOfWrongGuess;
+		println("You have " + remainChance + " guesses left.");
+	}
+
+	/*
+	 * Initial the default dash line.
+	 */
+	private String initDashLine(String word) {
+		String dash = "";
+		for (int i = 0; i < word.length(); i++) {
+			dash += "-";
+		}
+		return dash;
+	}
+
+	/*
+	 * Check whether the input is a character or not
+	 */
+	private boolean isValidGuess(String guess) {
+		return guess.length() == 1 && Character.isLetter(guess.charAt(0));
+	}
+
+	/*
+	 * To update game state.
+	 */
+	private void gameUpdate(char guess) {
+		int indexOfChar = word.indexOf(guess);
+		if (indexOfChar != -1) {
+			for (int i = 0; i < word.length(); i++) {
+				if (word.charAt(i) == guess) {
+					dashLine = dashLine.substring(0, i) + guess + dashLine.substring(i + 1);
+				}
+			}
+			println("That guess is correct.");
+		} else {
+			println("There are no " + guess + "'s in the word.");
+			numberOfWrongGuess++;
+		}
+	}
+
+	private boolean isWin() {
+		return !dashLine.contains("-");
+	}
+
+	private boolean isLose() {
+		return numberOfWrongGuess == NUMBER_OF_CHANCE;
+	}
+
+	private void winPrint() {
+		println("You guessed the word: " + word);
+		println("You win.");
+	}
+
+	private void losePrint() {
+		println("You're completely hung.");
+		println("The word was: " + word);
+		println("You lose.");
+	}
+
+	/* Private instance variables */
+	private HangmanLexicon lexicon = new HangmanLexicon();
+	private String dashLine;
+	private String word;
+	private int numberOfWrongGuess;
+	private static final int NUMBER_OF_CHANCE = 8;
+
+	// A canvas object used to draw a graphic
+	private HangmanCanvas canvas;
+}
